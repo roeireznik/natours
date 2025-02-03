d918cb13bf97a0ac28d44a227c140b9f83868939
Roei Reznik <roeireznik@gmail.com>
2025-02-03 18:43:43 +0200

    stripe

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { showAlert } from './alerts';
 
 export const bookTour = async (tourID) => {
+  console.log(tourID);
   try {
     const stripe = Stripe(
       'pk_test_51QmGflFb5ABD0L3C8ZaU8Rff17eYNdvsrCFsoF5E59X2xCdxJ82RZ9iN3Qy1TOpPLsaOZGiTtljocLgVRXuCVayv00pmkL5P4n',
