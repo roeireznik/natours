c2da9602553f671f7590730247b31f7549081d66
Roei Reznik <roeireznik@gmail.com>
2025-02-03 19:58:28 +0200

    debug booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -79,21 +79,16 @@ exports.webhookCheckout = (req, res, next) => {
   console.log('Webhook hit!');
 
   const signature = req.headers['stripe-signature'];
+  console.log('Stripe Signature:', signature);
+  console.log('Webhook Secret:', process.env.STRIPE_WEBHOOK_SECRET);
   let event;
   try {
-    const body = JSON.parse(req.body.toString('utf8')); // Parse the buffer to JSON
-    console.log('Parsed request body:', body);
-    const id = body.data.object.id; // Or body.id, body.data.id, etc. - check your logs!
-    console.log('ID:', id);
-    if (!id) {
-      console.error('ID is missing in webhook payload!');
-      return res.status(400).send('ID is required.');
-    }
     event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET,
     );
+    console.log('Constructed Event:', event); // Log the *entire* event
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
